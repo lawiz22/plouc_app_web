bd9bc57866a3042fddbf3e2df36728fa7662fecc
louis martin richard <lawiz2222@gmail.com>
2019-06-30 17:52:44 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,7 @@ module.exports = {
     
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public'),
     filename: 'js/bundle.js',
   },
   devtool: 'source-map',
